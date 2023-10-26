24bf766614c7ca0ce13d8104dacaac6f4b77124a
Gouthamkumar2011 <gouthamthilaga@gmail.com>
2023-10-26 15:39:11 -0400

    LL_Prac_reverse

---
+++ LL_reverse.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self,value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+    def reverse(self):
+        temp =self.head
+        self.head = self.tail
+        self.tail = temp
+        after = temp.next
+        before = None
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
+
+
+my_Linked_List = LinkedList(1)
+my_Linked_List.append(2)
+my_Linked_List.append(3)
+my_Linked_List.append(4)
+my_Linked_List.reverse()
+my_Linked_List.print_list()
+
