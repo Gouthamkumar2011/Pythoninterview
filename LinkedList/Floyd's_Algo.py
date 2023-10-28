563b154a226ea9f3143a527c770fe3dd24413a30
Gouthamkumar2011 <gouthamthilaga@gmail.com>
2023-10-27 21:01:00 -0400

    Flloyd(tortoise & Hare)

---
+++ LinkedList/Floyd's_Algo.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+    
+    def printList(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node =Node (value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+
+    def has_loop(self):
+        slow = self.head
+        fast = self.head
+        while fast.next and fast.next.next is not None:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+        return False
+    
+my_linked_list_1 = LinkedList(1)
+my_linked_list_1.append(2)
+my_linked_list_1.append(3)
+my_linked_list_1.append(4)
+my_linked_list_1.tail.next = my_linked_list_1.head
+print(my_linked_list_1.has_loop() )
