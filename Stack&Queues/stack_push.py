6d4fd619026842432430b243946fe5db0073b7f5
Gouthamkumar2011 <gouthamthilaga@gmail.com>
2023-10-29 12:50:35 -0400

    StackPush_LL&List

--- Stack&Queues/stack_push.py
+++ Stack&Queues/stack_push.py
@@ -1,3 +1,4 @@
+#Stack_Push using Linked LISTs.
 class Node:
     def __init__(self,value):
         self.value=value
@@ -29,4 +30,28 @@ class Stack:
 
 my_stack = Stack(4)
 my_stack.push(1)
+my_stack.print_stack()
+
+#Stack_Push using LISTs
+
+class Stack:
+    def __init__(self):
+        self.stack_list = []
+        
+    def print_stack(self):
+        for i in range(len(self.stack_list)-1, -1, -1):
+            print(self.stack_list[i])
+
+    def push(self,value):
+        self.stack_list.append(value)
+    
+    
+
+
+
+my_stack = Stack()
+my_stack.push(1)
+my_stack.push(2)
+my_stack.push(3)
+
 my_stack.print_stack()
