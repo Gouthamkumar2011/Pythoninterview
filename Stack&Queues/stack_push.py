4f8183f06c45b025d51e891b108398b92e44b81e
Gouthamkumar2011 <gouthamthilaga@gmail.com>
2023-10-30 00:41:50 -0400

    push

--- Stack&Queues/stack_push.py
+++ Stack&Queues/stack_push.py
@@ -30,28 +30,29 @@ class Stack:
 
 my_stack = Stack(4)
 my_stack.push(1)
+my_stack.push(3)
 my_stack.print_stack()
 
 #Stack_Push using LISTs
 
-class Stack:
-    def __init__(self):
-        self.stack_list = []
+# class Stack:
+#     def __init__(self):
+#         self.stack_list = []
         
-    def print_stack(self):
-        for i in range(len(self.stack_list)-1, -1, -1):
-            print(self.stack_list[i])
+#     def print_stack(self):
+#         for i in range(len(self.stack_list)-1, -1, -1):
+#             print(self.stack_list[i])
 
-    def push(self,value):
-        self.stack_list.append(value)
+#     def push(self,value):
+#         self.stack_list.append(value)
     
     
 
 
 
-my_stack = Stack()
-my_stack.push(1)
-my_stack.push(2)
-my_stack.push(3)
+# my_stack = Stack()
+# my_stack.push(1)
+# my_stack.push(2)
+# my_stack.push(3)
 
-my_stack.print_stack()
+# my_stack.print_stack()
