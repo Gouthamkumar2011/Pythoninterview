45275cd546c9e660a7f6f0fcc50cd802bfa6f0d5
Gouthamkumar2011 <gouthamthilaga@gmail.com>
2023-10-31 15:41:02 -0400

    HS_keys

--- HashTable/HS_Keys.py
+++ HashTable/HS_Keys.py
@@ -2,18 +2,21 @@ class HashTable:
     def __init__(self,size=7):
         self.data_map = [None] * size
     
+    #Time Complexity: O(1)
     def __hash(self,key):
         my_hash = 0
         for letter in key:
             my_hash = (my_hash + ord(letter)*23) % len(self.data_map)
         return my_hash
     
+    #Time Complexity: O(1)
     def set_item(self,key,value):
         index= self.__hash(key)
         if self.data_map[index] ==None:
             self.data_map[index] = []
         self.data_map[index].append([key,value])
-    
+
+    #Time Complexity: O(1)
     def get_item(self,key):
         index = self.__hash(key)
         if self.data_map[index] is not None:
@@ -22,6 +25,7 @@ class HashTable:
                     return self.data_map[index][i][1]
         return None
     
+    #Time Complexity: O(1)
     def keys(self):
         all_keys = []
         for i in range (len(self.data_map)):
