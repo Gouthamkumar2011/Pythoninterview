e04299547a005fcae40f7c7e353a55bd235013b6
Gouthamkumar2011 <gouthamthilaga@gmail.com>
2023-11-06 13:38:25 -0500

    anagram

---
+++ general_interview/group_anagram.py
@@ -0,0 +1,16 @@
+def group_anagrams(strings):
+    anagram_groups = {}
+    for string in strings:
+        canonical = "".join(sorted(string))
+        if canonical in anagram_groups:
+            anagram_groups[canonical].append(string) 
+        else:
+            anagram_groups[canonical] = [string]
+    return list(anagram_groups.values())
+
+print("1st set:")
+print( group_anagrams(["eat", "tea", "tan", "ate", "nat", "bat"]) )
+
+
+
+
