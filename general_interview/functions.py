1f23c706d125c4f053cac88c2f7c203f0ec157f1
Gouthamkumar2011 <gouthamthilaga@gmail.com>
2023-12-19 13:07:24 -0500

    general

--- general_interview/functions.py
+++ general_interview/functions.py
@@ -1,7 +1,11 @@
-def plus_one(number):
-    def add_one(number):
-        return number + 1
+# def plus_one(number):
+#     def add_one(number):
+#         return number + 1
 
-    result = add_one(number)
-    return result
-print(plus_one(4))
+#     result = add_one(number)
+#     return result
+# print(plus_one(4))
+
+
+array = [1,2,3,4,5,6,7,8,9,10]
+print(len(array) - 1)
