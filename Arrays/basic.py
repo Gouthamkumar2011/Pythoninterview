96c34e7a835556cb0c8294effa8d7cc4349598b7
Gouthamkumar2011 <gouthamthilaga@gmail.com>
2026-07-15 18:12:30 +0530

    practice_leetcode

--- Arrays/basic.py
+++ Arrays/basic.py
@@ -1,7 +1,38 @@
-def linearsearch(arr, target):
-    for i in range(len(arr)):
-        if arr[i] == target:
-            return i
-    return -1
+# def longestcommonprefix(strs):
+#     res = ""
+#     for i in range(len(strs[0])):
+#         for s in strs:
+#             if i == len(s) or s[i] != strs[0][i]:
+#                 return res
+#         res += strs[0][i]
+#     return res 
 
-print(linearsearch([1,2,3,4,5],5))
+# print(longestcommonprefix(["flower","flow","flight"]))
+
+
+# def removeDuplicates(nums):
+#     l=1
+#     for r in range (1,len(nums)):
+#         if nums[r] != nums[r-1]:
+#             nums[l] = nums[r]
+#             l+=1
+#     return nums[:l]
+
+# print(removeDuplicates([1,1,2]))
+
+
+def searchInsert ( nums, target):
+        l,r = 0, len(nums) - 1
+        while l<=r:
+            mid = (l+r) // 2
+
+            if target == nums[mid]:
+                return mid
+            
+            if target > nums[mid]:
+                l = mid + 1
+            if target <nums[mid]:
+                r = mid - 1
+        return l
+
+print(searchInsert([1,3,5,6], 2))
---
+++ Arrays/debug.py
@@ -0,0 +1,4 @@
+strs = ["flower","flow","flight"]
+prefix = strs[0]
+print(len(prefix))
+print(strs[2][2])
