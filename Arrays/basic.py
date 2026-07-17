e089da6b6ae750f9f2d17e5aff3af2a04fdd96a7
Gouthamkumar2011 <gouthamthilaga@gmail.com>
2026-07-17 10:13:51 +0530

    LC_Preparations

--- Arrays/basic.py
+++ Arrays/basic.py
@@ -21,18 +21,31 @@
 # print(removeDuplicates([1,1,2]))
 
 
-def searchInsert ( nums, target):
-        l,r = 0, len(nums) - 1
-        while l<=r:
-            mid = (l+r) // 2
+# def searchInsert ( nums, target):
+#         l,r = 0, len(nums) - 1
+#         while l<=r:
+#             mid = (l+r) // 2
 
-            if target == nums[mid]:
-                return mid
+#             if target == nums[mid]:
+#                 return mid
             
-            if target > nums[mid]:
-                l = mid + 1
-            if target <nums[mid]:
-                r = mid - 1
-        return l
+#             if target > nums[mid]:
+#                 l = mid + 1
+#             if target <nums[mid]:
+#                 r = mid - 1
+#         return l
 
-print(searchInsert([1,3,5,6], 2))
+# print(searchInsert([1,3,5,6], 2))
+
+
+def plusOne(digits):
+        for i in range (len(digits)-1,-1,-1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            
+            digits[i] = 0
+        
+        return [1] + digits
+
+print(plusOne([9,9,9,9,9]))
