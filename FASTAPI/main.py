cfed60ea4f16090fb771f5fb85962c26f8098ca2
Gouthamkumar2011 <gouthamthilaga@gmail.com>
2026-07-31 10:41:00 +0530

    FASTAPI

---
+++ FASTAPI/main.py
@@ -0,0 +1,5 @@
+from enemy import *
+enemy = Enemy('Zombie',15,45)
+
+print(enemy.attack_damage)
+
---
+++ FASTAPI/string.py
@@ -0,0 +1,11 @@
+# birthday = int(input("How many days to birthday?"))
+# print(round(birthday/7))
+
+
+
+zoo = ['LION',"TIGER","ZEBRA","CHEETAH","PANTHER"]
+zoo.pop(3)
+zoo.append("ELEPHANT")
+zoo.pop(0)
+print(zoo)
+print(zoo[:3])
